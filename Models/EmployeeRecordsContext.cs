7163a480b8717ec1cdc29ccca191d3c3518ffd63
Tahsin <tmsifat921@gmailcom>
2021-12-09 12:46:10 +0600

    git

---
+++ Models/EmployeeRecordsContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApiCRUD.Models
+{
+    public class EmployeeRecordsContext:DbContext
+    {
+        public EmployeeRecordsContext(DbContextOptions<EmployeeRecordsContext> options)
+           : base(options)
+        {
+        }
+
+        public DbSet<Employee> Employee { get; set; } = null!;
+    }
+}
