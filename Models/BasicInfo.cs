212f2c32c627b8f824b686cc6098fa1c1d8c7cc8
Tahsin <tmsifat921@gmailcom>
2021-12-09 12:49:50 +0600

    dsf

---
+++ Models/BasicInfo.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApiCRUD.Models
+{
+    public class EmployeeBasicInfo
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Designation { get; set; }
+        public string Salary { get; set; }
+        public string BankAcc { get; set; }
+    }
+}
